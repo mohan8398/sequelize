1103c47f4438d6e4926b273ebdfe0d22d5f71669
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-08 18:35:39 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const User=require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors()); 
+
+app.use((req,res,next)=>{
+  User.findByPk(1)
+  .then(user=>{
+      req.user =user;
+      next();
+  })
+  .catch(err=>{console.log(err)});
+})
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .then(user=>{
+    if(!user){
+        return User.create({name: 'mohan', email:'mohan@gmail.com'});
+    }
+    return user;
+})
+  .catch(err => {
+    console.log(err);
+  });
