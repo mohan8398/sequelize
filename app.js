2cce358146860cc7b663734b2cdf68159f134f4b
mohan8398 <98051444+mohan8398@users.noreply.github.com>
2022-04-08 19:50:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product=require('./models/product');
 const User=require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
